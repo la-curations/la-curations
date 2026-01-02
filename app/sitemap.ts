dd9d0dd5b584a3703db69e377d18505830f09cc1
Leo Antony <leoantony20025@gmail.com>
2026-01-02 21:03:10 +0530

    sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -22,6 +22,12 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 0.5,
     },
     {
+      url: `${baseUrl}/theater/privacy-policy`,
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.5,
+    },
+    {
       url: `${baseUrl}/terms-of-use`,
       lastModified: new Date(),
       changeFrequency: "yearly",
