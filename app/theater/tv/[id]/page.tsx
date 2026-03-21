51df7840cc0ba9e909402c5e380316afbf6bbdba
leoantonytacbot <leo.antony@tacbot.com>
2026-03-21 17:17:00 +0530

    Query Strings in Share

--- app/theater/tv/[id]/page.tsx
+++ app/theater/tv/[id]/page.tsx
@@ -1,10 +1,32 @@
 import TheaterInvite from "@/components/theater/TheaterInvite";
-import { Metadata } from "next";
+import { Metadata, ResolvingMetadata } from "next";
 
-export const metadata: Metadata = {
-  title: "Theater App - Get the Movie & TV Tracker",
-  description: "Experience the next level of streaming with Theater App. Download now on Google Play.",
+type Props = {
+  params: Promise<{ id: string }>;
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
 };
+
+export async function generateMetadata(
+  { searchParams }: Props,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  const unresolvedSearchParams = await searchParams;
+  const title = (unresolvedSearchParams?.title as string) || "Theater App - Get the Movie & TV Tracker";
+  const description =
+    (unresolvedSearchParams?.description as string) ||
+    "Experience the next level of streaming with Theater App. Download now on Google Play.";
+  const image = unresolvedSearchParams?.image as string;
+
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      images: image ? [image] : [],
+    },
+  };
+}
 
 export default function TVInvitePage() {
   return <TheaterInvite />;
