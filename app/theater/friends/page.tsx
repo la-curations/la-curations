f69ddf3beb26f33ecbe976bc0eeff80b5294973e
leoantonytacbot <leo.antony@tacbot.com>
2026-06-04 16:16:51 +0530

    Theater | Freinds upd

--- app/theater/friends/page.tsx
+++ app/theater/friends/page.tsx
@@ -1,9 +1,17 @@
 import TheaterInvite from "@/components/theater/TheaterInvite";
 import { Metadata, ResolvingMetadata } from "next";
 
+type Props = {
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+};
 
-export async function generateMetadata(): Promise<Metadata> {
-  const title = "Theater App - Friends Invite";
+export async function generateMetadata(
+  { searchParams }: Props,
+): Promise<Metadata> {
+  const unresolvedSearchParams = await searchParams;
+  const title =
+    (unresolvedSearchParams?.t as string) ||
+    "Theater App - Friends Invite";
   const description =
     "Join your friends on Theater App and compare your diary and watchlists.";
 
